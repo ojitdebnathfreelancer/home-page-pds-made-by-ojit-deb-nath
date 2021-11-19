ad84eb2fea4d898cf60f30f307add330e9bc13dc
ojitdebnath0 <ojitdebnath0@gmail.com>
2021-11-19 09:32:49 -0700

    counter up update

--- js/custom.js
+++ js/custom.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
     $(".back-top").on("click",function(){
         $("html,body").animate({
             scrollTop:"0px",
-        },1500);
+        },1000);
     })
 
     // chat typeing 
@@ -45,6 +45,11 @@ $(document).ready(function(){
 
 $('.item').directionalHover({
     overlay:"overlay",
+});
+
+$('.counter').counterUp({
+    delay: 20,
+    time: 2000,
 });
 
 
